fa847fbb9b86b9bdd8bbd0133e4354911546c7ad
albert deng <albertdeng@kkbox.com>
2019-07-22 18:34:01 +0800

    loading effect

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+
+     <loading :active.sync="isLoading"></loading>
+
     <div class="text-right mt-4">
       <button class="btn btn-primary" data-toggle="modal" @click="openModal(true)">建立新產品</button>
     </div>
@@ -59,7 +62,7 @@
                     </div>
                     <div class="form-group">
                       <label for="customFile">或 上傳圖片
-                        <i class="fas fa-spinner fa-spin"></i>
+                        <i class="fas fa-spinner fa-spin" v-if="status.fileUploading"></i>
                       </label>
                       <input type="file" id="customFile" class="form-control"
                         ref="files" @change="uploadFile">
@@ -180,15 +183,21 @@ export default {
     return {
       products: [],
       tempProducts:{},
-      isNew: false
+      isNew: false,
+      isLoading: false,
+      status: {
+        fileUploading: false
+      }
     }
   },
   methods: {
     getProducts(){
       const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
       const vm = this;
+      vm.isLoading = true;
       this.$http.get(api).then((response) => {
         console.log(response.data);
+        vm.isLoading = false;
         vm.products = response.data.products;
       });
     },
@@ -248,12 +257,15 @@ export default {
       const formData = new FormData()
       formData.append('file-to-upload', uploadedFile);
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/upload`;
+      vm.status.fileUploading = true
+
       this.$http.post(url, formData ,{
         headers: {
           'Content-Type': 'multipart/form-data',
         }
       }).then((response)=>{
         console.log(response.data);
+        vm.status.fileUploading = false
         if(response.data.success){
           // vm.tempProducts.imageUrl = response.data.imageUrl
           vm.$set(vm.tempProducts, 'imageUrl', response.data.imageUrl)
